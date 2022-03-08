d77002ef78deb697177740ce431a6f9fbb72d723
zubinphp <zubinphp@gmail.com>
2022-03-08 09:35:15 +0530

    Formatted code

--- src/Rectangle.php
+++ src/Rectangle.php
@@ -1,21 +1,34 @@
 <?php
+
 namespace App;
 
-class Rectangle{
+class Rectangle
+{
 
     public float $length;
     public float $breadth;
 
-    public function getArea(){
-        return $this->length*$this->breadth;
+    public function getArea()
+    {
+
+        return $this->length * $this->breadth;
     }
 
-    public function getPerimeter(){
-        return 2*($this->length+$this->breadth);
+    public function getPerimeter()
+    {
+
+        return 2 * ($this->length + $this->breadth);
     }
-    
-    public function getDateTime(){
+
+    public function getDateTime()
+    {
 
         return date('Y-m-d H:i:s');
+    }
+
+    public function getDiagonal()
+    {
+
+        return sqrt(($this->length * $this->length) + ($this->breadth * $this->breadth));
     }
 }
